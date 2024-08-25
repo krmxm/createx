7e4d622ecdb09e5d6b3959e79f7076e53e7ceccc
Kramarenko Maxim <kramarenko-90@inbox.ru>
2024-08-25 18:21:26 +0300

    создал секцию recent-news

---
+++ src/layouts/recent-news/recent-news.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+
+import './recent-news.scss';
+
+class RecentNews extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    formatData = (dataString) => {
+        const data = new Date(dataString);
+
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return data.toLocaleDateString('en-US', options);
+    }
+
+    onRenderArticle = (news) => {
+        return news.map(({ id, recentArticleImg, recentArticleLinkText, recentArticleMetaLinkText, recentArticleMetaTime, recentArticleMetaComments, recentArticleDescr }, index) => {
+            if (index === 0) {
+                return (
+                    <li key={id} className="recent-news__item">
+                        <article className="recent-article">
+                            <img className='recent-article__img' src={recentArticleImg} alt="img news" />
+                            <div className="recent-article__text">
+                                <h3 className="lead lead_bold recent-article__title">
+                                    <a href="#">{recentArticleLinkText}</a>
+                                </h3>
+                                <div className="recent-article__meta article-meta small small_regular">
+                                    <a className='article-meta__item' href="#">{recentArticleMetaLinkText}</a>
+                                    <time className='article-meta__item' >{this.formatData(recentArticleMetaTime)}</time>
+                                    <span className='article-meta__item article-meta__item_comments'>{recentArticleMetaComments} comments</span>
+                                </div>
+                                <p className="recent-article__descr base base_regular">{recentArticleDescr}</p>
+                            </div>
+                        </article>
+
+                    </li>
+                )
+            } else if (0 < index && index <= 2) {
+                return (
+                    <li key={id} className="recent-news__item">
+                        <article className="recent-article recent-article_small">
+                            <img className='recent-article__img' src={recentArticleImg} alt="img news" />
+                            <div className="recent-article__text">
+                                <h3 className="lead lead_bold recent-article__title">
+                                    <a href="#">{recentArticleLinkText}</a>
+                                </h3>
+                                <div className="recent-article__meta article-meta small small_regular">
+                                    <a className='articlesmalla__item' href="#">{recentArticleMetaLinkText}</a>
+                                    <time className='article-meta__item' >{this.formatData(recentArticleMetaTime)}</time>
+                                    <span className='article-meta__item article-meta__item_comments'>{recentArticleMetaComments} comments</span>
+                                </div>
+                            </div>
+                        </article>
+
+                    </li>
+                )
+            } else {
+                return null;
+            }
+
+        })
+    }
+
+    render() {
+        const { recentNews } = this.props;
+        return (
+            <div className="recent-news section-offset">
+                <div className="container">
+                    <h2 className='title title_regular title_regular-center recent-news__title'>Recent news</h2 >
+                    <ul className="recent-news__list">
+                        {this.onRenderArticle(recentNews)}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default RecentNews;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,6 +9,7 @@ import PortfolioSection from '../../layouts/porfolio-section/portfolio-section';
 import PartnersSection from '../../layouts/partners-section/partners-section';
 import Testimonials from '../../layouts/testimonials/testimonials';
 import Facts from '../../layouts/facts/facts';
+import RecentNews from '../../layouts/recent-news/recent-news';
 
 import './HomePage.scss';
 
@@ -16,12 +17,21 @@ import './HomePage.scss';
 class HomePage extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            testimonialsSlides: [
-                { id: 1, authorImg: require('../../assets/img/testmonials-author.png'), descr: 'Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco.', authorName: 'Shawn Edwards', authorPosition: 'Position, Company name' },
-                { id: 2, authorImg: require('../../assets/img/testmonials-author.png'), descr: 'Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco.', authorName: 'John Smite', authorPosition: 'Position, Company name' }
-            ]
-        }
+        this.testimonialsSlides = [
+            { id: 1, authorImg: require('../../assets/img/testmonials-author.png'), descr: 'Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco.', authorName: 'Shawn Edwards', authorPosition: 'Position, Company name' },
+            { id: 2, authorImg: require('../../assets/img/testmonials-author.png'), descr: 'Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco.', authorName: 'John Smite', authorPosition: 'Position, Company name' }
+        ];
+        this.recentNews = [
+            { id: 1, recentArticleImg: require('../../assets/img/recent-article/recent-article-1.jpeg'), recentArticleLinkText: 'How to Build Climate Change-Resilient Infrastructure', recentArticleMetaLinkText: 'Industry News', recentArticleMetaTime: '2020-06-24 19:00', recentArticleMetaComments: '4', recentArticleDescr: 'Ipsum aliquet nisi, hendrerit rhoncus quam tortor, maecenas faucibus. Tincidunt aliquet sit vel, venenatis nulla. Integer bibendum turpis convallis enim, nibh convallis Ipsum aliquet nisi, hendrerit rhoncus quam tortor' },
+
+            { id: 2, recentArticleImg: require('../../assets/img/recent-article/recent-article-2.jpeg'), recentArticleLinkText: 'How Construction Can Help Itself', recentArticleMetaLinkText: 'Industry News', recentArticleMetaTime: '2020-06-24 19:00', recentArticleMetaComments: 'No', recentArticleDescr: 'Ipsum aliquet nisi, hendrerit rhoncus quam tortor, maecenas faucibus. Tincidunt aliquet sit vel, venenatis nulla. Integer bibendum turpis convallis enim, nibh convallis Ipsum aliquet nisi, hendrerit rhoncus quam tortor' },
+
+            { id: 3, recentArticleImg: require('../../assets/img/recent-article/recent-article-3.jpeg'), recentArticleLinkText: 'Types of Flooring Materials', recentArticleMetaLinkText: 'Industry News', recentArticleMetaTime: '2020-06-24 19:00', recentArticleMetaComments: 'No', recentArticleDescr: 'Ipsum aliquet nisi, hendrerit rhoncus quam tortor, maecenas faucibus. Tincidunt aliquet sit vel, venenatis nulla. Integer bibendum turpis convallis enim, nibh convallis Ipsum aliquet nisi, hendrerit rhoncus quam tortor' },
+
+            { id: 4, recentArticleImg: require('../../assets/img/recent-article/recent-article-1.jpeg'), recentArticleLinkText: 'recent-article-1', recentArticleMetaLinkText: 'Industry News', recentArticleMetaTime: '2020-06-24 19:00', recentArticleMetaComments: '4', recentArticleDescr: 'Ipsum aliquet nisi, hendrerit rhoncus quam tortor, maecenas faucibus. Tincidunt aliquet sit vel, venenatis nulla. Integer bibendum turpis convallis enim, nibh convallis Ipsum aliquet nisi, hendrerit rhoncus quam tortor' }
+
+
+        ]
 
     }
 
@@ -36,8 +46,9 @@ class HomePage extends Component {
                 <ServicesSection />
                 <PortfolioSection />
                 <PartnersSection />
-                <Testimonials testimonialsSlides={this.state.testimonialsSlides} />
+                <Testimonials testimonialsSlides={this.testimonialsSlides} />
                 <Facts />
+                <RecentNews recentNews={this.recentNews} />
             </>
         )
     }
